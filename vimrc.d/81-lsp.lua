8d9570cc94067f40be3f15c3f9fd2ec8234ddc7e
Gregory Kudrenko <gragory.mail@gmail.com>
2025-10-01 14:27:42 +0200

    Update modules

--- vimrc.d/81-lsp.lua
+++ vimrc.d/81-lsp.lua
@@ -1,21 +1,41 @@
-local lspconfig = require'lspconfig'
-
-lspconfig.util.default_config = vim.tbl_extend(
-    "force",
-    lspconfig.util.default_config,
-    { autostart = false }
-)
-
-lspconfig.csharp_ls.setup{}
-lspconfig.clangd.setup{}
-lspconfig.pyright.setup{}
-lspconfig.bashls.setup{}
-lspconfig.powershell_es.setup{
-  bundle_path = vim.fn.MakeDevenvPath("powershell-es")
+-- common options applied to all servers
+local common = {
+  -- example: add your on_attach/capabilities here
+  -- on_attach = my_on_attach,
+  -- capabilities = my_capabilities,
 }
 
---make --always-make --dry-run \
--- | grep -wE 'gcc|g\+\+' \
--- | grep -w '\-c' \
--- | jq -nR '[inputs|{directory:".", command:., file: match(" [^ ]+$").string[1:]}]' \
--- > compile_commands.json
+-- list of servers and their specific options
+local servers = {
+  csharp_ls     = {},
+  clangd        = {},
+  pyright       = {},
+  bashls        = {},
+  powershell_es = {
+    bundle_path = vim.fn.MakeDevenvPath("powershell-es"),
+  },
+}
+
+-- check if the new API is available
+local has_new, newcfg = pcall(function() return vim.lsp.config end)
+local lsp = has_new and newcfg or require("lspconfig")
+
+if has_new then
+  -- new style: register configs (servers will be enabled automatically when needed)
+  for name, opts in pairs(servers) do
+    local merged = vim.tbl_deep_extend("force", {}, common, opts)
+    lsp[name] = merged
+  end
+
+else
+  -- old style: defaults can be set via util.default_config
+  lsp.util.default_config = vim.tbl_deep_extend("force", {}, lsp.util.default_config or {}, {
+    autostart = false,
+  })
+
+  for name, opts in pairs(servers) do
+    local merged = vim.tbl_deep_extend("force", {}, common, opts)
+    lsp[name].setup(merged)
+  end
+end
+
